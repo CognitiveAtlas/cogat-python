8141bbe71d1a698976ebd41c652ca4e70cba7ef3
vsoch <vsochat@stanford.edu>
2015-06-16 17:40:15 -0700

    updating theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -111,9 +111,12 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'alabaster'
+# Only import theme locally; on RTD, it's the default.
+# See http://read-the-docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
+if os.environ.get('READTHEDOCS', None) is None:
+  import sphinx_rtd_theme
+  html_theme = "sphinx_rtd_theme"
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
