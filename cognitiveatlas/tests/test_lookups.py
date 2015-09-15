8c47fba7c31d4c902ce2a664176ed49a89a49282
vsoch <vsochat@stanford.edu>
2015-09-14 18:08:53 -0700

    modified:   cognitiveatlas/tests/test_lookups.py

--- cognitiveatlas/tests/test_lookups.py
+++ cognitiveatlas/tests/test_lookups.py
@@ -18,14 +18,14 @@ def test_lookups():
                u'trm_4ebd44cd88360', u'trm_4f24126c22011',
                u'trm_551f151f7347e']
     lookup = get_contrast_lookup(task_id=task_id)
-    assert_true(len(concept_lookup)>20)
+    assert_true(len(lookup)>20)
 
     # Test for specific contrasts
     contrast_ids = [u'cnt_523c7cf0ea7b4',u'cnt_553a64ea67841',
                     u'cnt_553a664f97104',u'cnt_553a647ca67ed',
                     u'cnt_553a64d23c393',u'cnt_523c7d0de6735']
     lookup = get_contrast_lookup(contrast_id=contrast_ids)
-
+    assert_true(len(lookup)==6)
 
 @raises(ValueError)
 def test_raises_type_error():
