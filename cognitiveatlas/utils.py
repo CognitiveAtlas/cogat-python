8e4dbf918c9574952c524238bc0311ff47d5e8b6
Ross Blair <rosswilsonblair@gmail.com>
2024-02-07 13:21:01 -0600

    future install_aliases is broken in python3.12

--- cognitiveatlas/utils.py
+++ cognitiveatlas/utils.py
@@ -14,9 +14,6 @@ import json
 import errno
 import pandas
 
-from future.standard_library import install_aliases
-install_aliases()
-
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 
